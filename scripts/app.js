5c42261af7bb03bf3d4bead80fb2232b03f44c14
rohit kumar <rohitkumar38344@gmail.com>
2023-08-21 22:18:42 +0530

    feature: displays current active tasks

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 const taskOpSection = document.querySelector(".task-list-status");
+const taskCountEl = document.getElementById("task-count");
+
 document.addEventListener("keydown", function (e) {
   if (e.key == "Enter" && e.target.id === "user-input") {
     const userInput = e.target.value;
@@ -6,6 +8,16 @@ document.addEventListener("keydown", function (e) {
     createTask(userInput);
   }
 });
+
+function updateTaskStatus() {
+  const taskListEl = document.querySelectorAll(".task-list-item");
+  // console.log(taskListEl);
+  let activetaskCount = 0;
+  taskListEl.forEach((task) => {
+    if (task.classList.contains("active")) activetaskCount++;
+  });
+  taskCountEl.textContent = activetaskCount;
+}
 
 function updateTask(ev) {
   const targetEl = ev.target;
@@ -36,6 +48,7 @@ function createTask(taskInfo) {
   const divEl = document.createElement("div");
   divEl.classList.add("task-list-item", "active");
   divEl.addEventListener("click", updateTask);
+  divEl.addEventListener("click", updateTaskStatus);
 
   // add image when the task is checked as child of parent div
   const imgEl = `<img src="../images/icon-check.svg" alt="" class="w-full" />`;
@@ -52,6 +65,7 @@ function createTask(taskInfo) {
 
   //reseting the value
   document.querySelector("#user-input").value = "";
+  updateTaskStatus();
   // rebuildTailwind();
 }
 /*
